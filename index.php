9f0090a0a0a434120983bebc921b000e5b1711f4
Ash Blue <ash@ashbluewebdesign.com>
2012-07-17 19:35:19 -0500

    Commit for the majority of the completed code.

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 include('package_zipper.php');
 
 $zip_pack = new Zip_Pack;
-$zip_pack->set_file('foo.txt', 'foo bar');
-$zip_pack->set_file('foo.txt', 'bar foo');
-//$zip_pack->output_zip('blah');
+$zip_pack->clone_dir('demo');
+//$zip_pack->set_folder('blah');
+//$zip_pack->set_folder('test');
+//$zip_pack->delete_name('test/');
+$zip_pack->set_file('blah/foo.txt', 'foo bar');
+//$zip_pack->set_file('foo.txt', 'bar foo');
+$zip_pack->get_zip();
 ?>
--- package_zipper.php
+++ package_zipper.php
@@ -1,7 +1,8 @@
 <?php
 /* TODO:
-  - Add new folders
-  - Ability to clone an existing directory from the current location
+  - Finish cloning method
+  - Simple documentation with PHP docs
+  - Setup demo files
 */
 
 class Zip_Pack {
@@ -10,21 +11,16 @@ class Zip_Pack {
     var $zip_package = null; // The complete zip package
 
 	function __construct() {
-		$this->zip_extension_exists();
+		$this->zip_support();
 
         // Zip interface object http://www.php.net/manual/en/class.ziparchive.php
         $this->zip = new ZipArchive();
 
-        // Create a zip file with a temporary name
-        $this->zip_package = tempnam($this->get_temp_dir(), 'zip_package');
-
-        // Create an empty zip file to store the data and open the interface
-        $this->zip->open($this->zip_package, ZipArchive::CREATE);
+        $this->create_zip();
 	}
 
-	// Test and error handling for zip extension support in the current running
-	// server.
-	function zip_extension_exists() {
+	// Verify current hosting environment supports the PHP Zip extension.
+	function zip_support() {
 		if (!extension_loaded('zip')) {
 			$error_message = 'Package Zipper requires the PHP Zip extension to be enabled. For information on enabling it please see the official PHP Zip extenstion installation docs at http://www.php.net/manual/en/zip.setup.php';
 			throw new Exception($error_message);
@@ -33,29 +29,43 @@ class Zip_Pack {
 		}
 	}
 
-    function output_zip($name) {
-        // Close the zip interface
-        $this->zip->close();
+	// Creates a new zip file for output, good for overriding old zip data
+	function create_zip() {
+        // Create a zip file with a temporary name
+        $this->zip_package = tempnam($this->get_temp_dir(), 'zip_package');
 
-        // Retrieve file size so the browser knows the length of the download
-        $file_size = filesize($this->zip_package);
+        // Create an empty zip file to store the data and open the interface
+        $this->zip->open($this->zip_package, ZipArchive::CREATE);
+	}
 
-        // Output proper header data to force a download instead of going to a new page
-        header('Content-Type: application/zip');
-        header('Content-Length: ' . $file_size);
-        header('Content-Disposition: attachment; filename="' . $name . '.zip"');
+	// Returns a file instead of a page from the current PHP file
+    function get_zip($name = null) {
+		// Close the zip interface
+		$this->zip->close();
 
-        // Send the user a downloadable file
-        readfile($this->zip_package);
+		if ($name !== null) {
+			// Retrieve file size so the browser knows the length of the download
+			$file_size = filesize($this->zip_package);
+
+			// Output proper header data to force a download instead of going to a new page
+			header('Content-Type: application/zip');
+			header('Content-Length: ' . $file_size);
+			header('Content-Disposition: attachment; filename="' . $name . '.zip"');
+
+			// Send the user a downloadable file
+			readfile($this->zip_package);
+		} else {
+			return $this->zip_package;
+		}
     }
 
     /**
 	 * Creates a new file inside the current zip archive. If you supply an existing
 	 * file name and location, the existing file will be overwritten.
 	 */
-    function set_file($name, $content, $loc = '') {
+    function set_file($name, $content) {
         // Create temporary file
-        $file = tempnam($this->get_temp_dir(), $name . $loc);
+        $file = tempnam($this->get_temp_dir(), $name);
 
         // Generate a simple read and write utility in binary
         $file_writer = fopen($file, "w"); // Opens the file with a write utility
@@ -67,14 +77,33 @@ class Zip_Pack {
     }
 
 	// Set or override an existing folder
-	function set_folder($name, $loc = '') {
+	function set_folder($loc) {
+		$this->zip->addEmptyDir($loc);
+	}
 
+	// Delete an existing file or folder
+	// NOTE: All folder paths must end with a "/", for example to delete a folder called
+	// foo, you must pass 'foo/' as $loc
+	function delete_name($loc) {
+		$this->zip->deleteName($loc);
 	}
 
     // Clones a directory into the zip file
 	// http://stackoverflow.com/questions/1334613/how-to-recursively-zip-a-directory-in-php
-    function clone_dir($loc) {
+    function clone_dir($loc, $include_parent_folder = false) {
+		$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($loc), RecursiveIteratorIterator::SELF_FIRST);
 
+		foreach ($files as $file) :
+			if (is_dir($file) === true):
+				echo 'dir ';
+				// $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+			elseif (is_file($file) === true):
+				echo 'file ';
+				// $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+			endif;
+
+			echo $file . '<br>';
+		endforeach;
     }
 
     // Retrieve location of system's temporary directory
