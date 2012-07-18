fdff8d79fe5d161584ed35bac13d12d649ea6350
Ash Blue <ash@ashbluewebdesign.com>
2012-07-18 03:04:25 -0500

    Completion of coding, needs docs, testing, demos, ect.

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 include('package_zipper.php');
 
 $zip_pack = new Zip_Pack;
-$zip_pack->clone_dir('demo');
+$zip_pack->clone_dir('demo')
 //$zip_pack->set_folder('blah');
 //$zip_pack->set_folder('test');
 //$zip_pack->delete_name('test/');
-$zip_pack->set_file('blah/foo.txt', 'foo bar');
+//$zip_pack->set_file('blah/foo.txt', 'foo bar');
 //$zip_pack->set_file('foo.txt', 'bar foo');
-$zip_pack->get_zip();
+->get_zip('blah');
 ?>
+
+<!--
+    - Introduction
+    - Demo List
+    - Documentation (ReadMe and website link)
+-->
--- package_zipper.php
+++ package_zipper.php
@@ -1,16 +1,16 @@
 <?php
 /* TODO:
-  - Finish cloning method
-  - Simple documentation with PHP docs
+  - Simple documentation with PHP docs http://www.phpdoc.org/
   - Setup demo files
 */
 
 class Zip_Pack {
-    var $temp_loc = null; // Location of system's temporary directory
-    var $zip = null; // Zip interface object http://www.php.net/manual/en/class.ziparchive.php
-    var $zip_package = null; // The complete zip package
+    private static $error_message = 'Package Zipper requires the PHP Zip extension to be enabled. For information on enabling it please see the official PHP Zip extenstion installation docs at http://www.php.net/manual/en/zip.setup.php';
+    public $temp_loc = null; // Location of system's temporary directory
+    public $zip = null; // Zip interface object http://www.php.net/manual/en/class.ziparchive.php
+    public $zip_package = null; // The complete zip package
 
-    function __construct() {
+    public function __construct() {
         $this->zip_support();
 
         // Zip interface object http://www.php.net/manual/en/class.ziparchive.php
@@ -20,26 +20,37 @@ class Zip_Pack {
     }
 
     // Verify current hosting environment supports the PHP Zip extension.
-    function zip_support() {
-        if (!extension_loaded('zip')):
-            $error_message = 'Package Zipper requires the PHP Zip extension to be enabled. For information on enabling it please see the official PHP Zip extenstion installation docs at http://www.php.net/manual/en/zip.setup.php';
-            throw new Exception($error_message);
-        else:
-            return true;
-        endif;
+    private function zip_support() {
+        if (!extension_loaded('zip'))
+            throw new Exception($this->error_message);
+
+        return $this;
+    }
+
+    // Cleans and returns a string if active
+    private static function clean_string($string, $removed_string, $active = true) {
+        return $active ? $string : str_replace($removed_string . '\\', '', $string);
+    }
+
+    // Retrieve location of system's temporary directory
+    // Cache and return result when possible
+    private function get_temp_dir() {
+        return ($this->temp_loc !== null) ? $this->temp_loc : $this->temp_loc = sys_get_temp_dir();
     }
 
     // Creates a new zip file for output, good for overriding old zip data
-    function create_zip() {
+    public function create_zip() {
         // Create a zip file with a temporary name
         $this->zip_package = tempnam($this->get_temp_dir(), 'zip_package');
 
         // Create an empty zip file to store the data and open the interface
         $this->zip->open($this->zip_package, ZipArchive::CREATE);
+
+        return $this;
     }
 
     // Returns a file instead of a page from the current PHP file
-    function get_zip($name = null) {
+    public function get_zip($name = null) {
         // Close the zip interface
         $this->zip->close();
 
@@ -63,7 +74,7 @@ class Zip_Pack {
      * Creates a new file inside the current zip archive. If you supply an existing
      * file name and location, the existing file will be overwritten.
      */
-    function set_file($name, $content) {
+    public function set_file($name, $content) {
         // Create temporary file
         $file = tempnam($this->get_temp_dir(), $name);
 
@@ -74,45 +85,44 @@ class Zip_Pack {
 
         // Save file data for inclusion
         $this->zip->addFile($file, $name);
+
+        return $this;
     }
 
     // Set or override an existing folder
-    function set_folder($loc) {
+    public function set_folder($loc) {
         $this->zip->addEmptyDir($loc);
+
+        return $this;
     }
 
     // Delete an existing file or folder
     // NOTE: All folder paths must end with a "/", for example to delete a folder called
     // foo, you must pass 'foo/' as $loc
-    function delete_name($loc) {
+    public function delete_name($loc) {
         $this->zip->deleteName($loc);
+
+        return $this;
     }
 
     // Clones a directory into the zip file
-    // http://stackoverflow.com/questions/1334613/how-to-recursively-zip-a-directory-in-php
-    function clone_dir($loc, $include_parent_folder = false) {
-        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($loc), RecursiveIteratorIterator::SELF_FIRST);
+    public function clone_dir($loc, $include_parent_folder = true) {
+        $catalog = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($loc), RecursiveIteratorIterator::SELF_FIRST);
 
-        foreach ($files as $file) :
-            if (is_dir($file) === true):
-                echo 'dir ';
-                // $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
-            elseif (is_file($file) === true):
-                echo 'file ';
-                // $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+        // Look through all files retrieved
+        foreach ($catalog as $item):
+            // Clean and prep the recursive file data
+            $output = $this->clean_string($item, $loc, $include_parent_folder);
+
+            // Include directory data based upon file or directory discovery
+            if (is_dir($item) === true):
+                $this->zip->addEmptyDir($output);
+            elseif (is_file($item) === true):
+                $this->zip->addFromString($output, file_get_contents($item));
             endif;
-
-            echo $file . '<br>';
         endforeach;
-    }
 
-    // Retrieve location of system's temporary directory
-    function get_temp_dir() {
-        // Cache and return result when possible
-        if ($this->temp_loc !== null)
-            return $this->temp_loc;
-        else
-            return $this->temp_loc = sys_get_temp_dir();
+        return $this;
     }
 };
 ?>
